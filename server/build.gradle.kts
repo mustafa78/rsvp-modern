b48fe1f342760c66bf47680b588891df0fdb26a4
Mustafa Kanchwala <mustafa.kanchwala@gmail.com>
2025-08-14 00:18:41 -0400

    update the build and docker files for a clean run

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -17,7 +17,8 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.flywaydb:flyway-core")
+  implementation("org.flywaydb:flyway-core:11.10.0")
+  implementation("org.flywaydb:flyway-database-postgresql:11.10.0")
   runtimeOnly("org.postgresql:postgresql:42.7.4")
 
   testImplementation("org.springframework.boot:spring-boot-starter-test")
